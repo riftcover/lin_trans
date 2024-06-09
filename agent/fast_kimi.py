96f515178164a04f949b4e0fa972c210cd25f711
riftcover <44368727+riftcover@users.noreply.github.com>
2024-06-09 19:29:08 +0800

    1.添加智谱AI翻译字幕

---
+++ agent/fast_kimi.py
@@ -0,0 +1,47 @@
+import time
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="sk-ZBNavwnJtJzolfVLIxCPaBoYEYfwytXk0ezdo49pwQ3frjl6",
+    base_url="https://api.moonshot.cn/v1",
+)
+filter = 'data/5月24日.txt'
+out_file='data/finish.txt'
+def chat_translate(text):
+    completion = client.chat.completions.create(
+        model="moonshot-v1-8k",
+        messages=[
+            {"role": "system",
+             "content": """
+             你是一位精通简体中文的专业翻译，尤其擅长滑雪相关教学的翻译，我会给你一份英文文件，帮我把这段英文翻译成中文，提供给我完整的中文尽量保证中文内容的行数与英文文件一致.
+             """},
+            # {
+            #     "role": "system",
+            #     "content": file_content,
+            # },
+            {"role": "user", "content": text}
+        ],
+        temperature=0.3,
+    )
+    return completion.choices[0].message.content
+
+
+def translate_document(in_document,out_document, chunk_size=30):
+    with open(in_document, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    chunks = ["".join(lines[i:i + chunk_size]) for i in range(0, len(lines), chunk_size)]
+
+    with open(out_document, 'a', encoding='utf-8') as output_content:
+        for paragraph in chunks:
+            print(paragraph)
+            translated_paragraph = chat_translate(paragraph)
+            print(translated_paragraph)
+            # translated_paragraph = paragraph
+            output_content.writelines(translated_paragraph)
+            time.sleep(32)
+
+
+#
+translation = translate_document(filter,out_file)
+print(translation)
+
---
+++ tools/read_config.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+
+def load_api_key_from_config(key: str) -> str:
+    home_path = os.path.dirname(os.getcwd())
+
+    with open(f'{home_path}/config.json', 'r') as file:
+        config = json.load(file)
+        print(type(config))
+    if key not in config:
+        raise KeyError(f"Key '{key}' not found in the configuration file.")
+    return config.get(key)
+
+
+if __name__ == '__main__':
+    print(load_api_key_from_config('zhipu_key'))
