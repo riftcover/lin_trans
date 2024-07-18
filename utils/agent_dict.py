309b0b8ddd1bf848d3396ed926e7d7c807c8bac7
riftcover <gaoshoupan@hotmail.com>
2024-07-18 16:18:22 +0800

    add.添加agent_settings数据结构

---
+++ utils/agent_dict.py
@@ -0,0 +1,35 @@
+from typing import TypedDict, Dict
+
+from PySide6.QtCore import QSettings
+
+
+class AgentDict(TypedDict):
+    key: str
+    base_url: str
+    model: str
+
+
+AgentSettings = Dict[str, AgentDict]
+
+
+qwen_msg ={
+    "base_url": "https://dashscope.aliyuncs.com/compatible-mode/v1",
+    "model": "qwen2-57b-a14b-instruct"
+}
+
+kimi_msg = {
+    "base_url": "https://api.moonshot.cn/v1",
+    "model": "moonshot-v1-8k"
+}
+
+
+def agent_settings() -> AgentSettings:
+    gui_settings = QSettings("Locoweed", "LinLInTrans")
+    qwen_key = {"key": gui_settings.value("qwen")}
+    qwen_msg.update(qwen_key)
+    kimi_key = {"key": gui_settings.value("kimi")}
+    kimi_msg.update(kimi_key)
+    return {
+        "qwen": qwen_msg,
+        "kimi": kimi_msg
+    }
