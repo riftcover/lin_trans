ccd00c89f5b68ed1a8862133366a1ed384c32302
riftcover <44368727+riftcover@users.noreply.github.com>
2024-07-26 23:35:45 +0800

    style(srt_edit): 修改变量名出为下划线规则

--- nice_ui/ui/srt_edit.py
+++ nice_ui/ui/srt_edit.py
@@ -15,15 +15,15 @@ class SubtitleEditPage(CardWidget):
         layout = QVBoxLayout(self)
         self.subtitleTable = SubtitleTable(self.patt)
         # 创建并添加CardWidget
-        topCard = CardWidget()
-        topLayout = QHBoxLayout(topCard)
-        topLayout.setSpacing(10)
-        topLayout.setContentsMargins(10, 10, 10, 10)
+        top_card = CardWidget()
+        top_layout = QHBoxLayout(top_card)
+        top_layout.setSpacing(10)
+        top_layout.setContentsMargins(10, 10, 10, 10)
 
-        exportButton = QPushButton("导出")
-        exportButton.clicked.connect(self.export_srt)
-        saveButton = QPushButton("保存")
-        saveButton.clicked.connect(self.subtitleTable.save_subtitle)
+        export_button = QPushButton("导出")
+        export_button.clicked.connect(self.export_srt)
+        save_button = QPushButton("保存")
+        save_button.clicked.connect(self.subtitleTable.save_subtitle)
 
         down_rows = QPushButton("下移译文")
         # 创建并添加SubtitleTable
@@ -35,20 +35,20 @@ class SubtitleEditPage(CardWidget):
         # 向上移动按钮
 
         # 从右向左排列按钮
-        topLayout.addStretch(1)
-        topLayout.addWidget(down_rows)
-        topLayout.addWidget(up_rows)
-        topLayout.addWidget(saveButton)
-        topLayout.addWidget(exportButton)
+        top_layout.addStretch(1)
+        top_layout.addWidget(down_rows)
+        top_layout.addWidget(up_rows)
+        top_layout.addWidget(save_button)
+        top_layout.addWidget(export_button)
 
-        layout.addWidget(topCard)
+        layout.addWidget(top_card)
 
         layout.addWidget(self.subtitleTable)
 
-        exportButton.setToolTip("导出srt格式字幕文件")
-        exportButton.installEventFilter(ToolTipFilter(exportButton, showDelay=300, position=ToolTipPosition.BOTTOM_RIGHT))
-        saveButton.setToolTip("保存到本地，以免丢失")
-        saveButton.installEventFilter(ToolTipFilter(saveButton, showDelay=300, position=ToolTipPosition.BOTTOM_RIGHT))
+        export_button.setToolTip("导出srt格式字幕文件")
+        export_button.installEventFilter(ToolTipFilter(export_button, showDelay=300, position=ToolTipPosition.BOTTOM_RIGHT))
+        save_button.setToolTip("保存到本地，以免丢失")
+        save_button.installEventFilter(ToolTipFilter(save_button, showDelay=300, position=ToolTipPosition.BOTTOM_RIGHT))
         down_rows.setToolTip("将勾选的译文整体向下移动")
         down_rows.installEventFilter(ToolTipFilter(down_rows, showDelay=300, position=ToolTipPosition.BOTTOM_RIGHT))
         up_rows.setToolTip("将勾选的译文向上移动译文")
@@ -60,10 +60,6 @@ class SubtitleEditPage(CardWidget):
     def export_srt(self):
         # todo：导出srt文件
         print("export srt")
-
-    def save_srt(self):
-        # todo：保存srt文件
-        print("save srt")
 
 
 if __name__ == "__main__":
