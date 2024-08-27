ab81285b1a4622c6188ca012341b6a6346da29b9
riftcover <gaoshoupan@hotmail.com>
2024-08-27 13:09:33 +0800

    1.字幕编辑器：虚拟滚动创建 2.添加依赖

--- nice_ui/widget/subedit.py
+++ nice_ui/widget/subedit.py
@@ -1,74 +1,13 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QTableView, QStyledItemDelegate, QPushButton, QWidget, QVBoxLayout, QStyle, QStyleOptionButton, \
     QStyleOptionSpinBox, QStyleOptionViewItem, QTimeEdit, QTextEdit, QLabel
 from PySide6.QtCore import Qt, QAbstractTableModel, QModelIndex, QSize, QRect, QPoint, QTime, QSignalBlocker, QTimer, QAbstractListModel, QThread, Signal, \
-    QElapsedTimer
+    QElapsedTimer, QObject, QRunnable, Slot, QThreadPool
 from PySide6.QtGui import QBrush, QColor, QStandardItemModel, QRegion
 import sys
 
 from nice_ui.configure import config
 from nice_ui.ui.style import LinLineEdit, LTimeEdit
 from vendor.qfluentwidgets import FluentIcon, CheckBox, TransparentToolButton, ToolTipFilter, ToolTipPosition
-
-
-class DataLoader(QThread):
-    data_loaded = Signal(list)
-
-    def __init__(self, file_path):
-        super().__init__()
-        self.file_path = file_path
-
-    def run(self):
-        data = self.load_subtitle()
-        self.data_loaded.emit(data)
-
-    def load_subtitle(self):
-        """
-        加载字幕文件，返回字幕列表
-        :return:[('00:00:00,166', '00:00:01,166', '你好，世界！')]
-                [start_time, end_time, content]
-        """
-        subtitles = []
-        with open(self.file_path, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
-
-        i = 0
-        while i < len(lines):
-            # 跳过行号
-            if lines[i].strip().isdigit():
-                i += 1
-
-            # 读取时间范围
-            time_range = lines[i].strip()
-            try:
-                start_time, end_time = time_range.split(' --> ')
-            except ValueError:
-                config.logger.error(f"Error parsing time range: {time_range}")
-                i += 1
-                continue
-            i += 1
-
-            # 读取字幕内容
-            english_content = []
-            if i < len(lines) and lines[i].strip():
-                english_content.append(lines[i].strip())
-                i += 1
-
-            chinese_content = []
-            if i < len(lines) and lines[i].strip():
-                chinese_content.append(lines[i].strip())
-                i += 1
-
-            english_content_str = ' '.join(english_content)
-            chinese_content_str = ' '.join(chinese_content)
-
-            # 添加到字幕列表
-            subtitles.append((start_time, end_time, english_content_str, chinese_content_str))
-
-            # 跳过空行
-            while i < len(lines) and not lines[i].strip():
-                i += 1
-        config.logger.debug(f"字幕文件行数: {len(subtitles)}")
-        return subtitles
 
 
 class CustomItemDelegate(QStyledItemDelegate):
@@ -78,25 +17,82 @@ class CustomItemDelegate(QStyledItemDelegate):
         # 构造函数，初始化父类，创建一个持久化编辑器字典
         super().__init__(parent)
         self._persistent_editors = {}
+        self.signals = VirtualScrollSignals()
 
     def createEditor(self, parent, option, index):
+        """
+        创建单元格编辑器，根据单元格的列数不同，创建不同的编辑器
+        目前支持的编辑器有：勾选框、操作按钮、行号、时间、原文、译文、编辑按钮
+        self.signals.createEditor.emit(index.row(), index.column()) 用来通知哪个单元格需要创建编辑器
+        Args:
+            parent:
+            option:
+            index:
+
+        Returns:
+
+        """
         if index.column() == 0:  # 勾选框
+            self.signals.createEditor.emit(index.row(), index.column())
             return self.create_checkbox(parent)
         elif index.column() == 1:  # 操作按钮
+            self.signals.createEditor.emit(index.row(), index.column())
             return self.create_operation_widget(parent)
         elif index.column() == 2:  # 行号
+            self.signals.createEditor.emit(index.row(), index.column())
             return self.create_row_number_label(parent, index.row())
         elif index.column() == 3:  # 时间
+            self.signals.createEditor.emit(index.row(), index.column())
             return self.create_time_widget(parent)
         elif index.column() in [4, 5]:  # 原文和译文
+            self.signals.createEditor.emit(index.row(), index.column())
             return self.create_text_edit(parent)
         elif index.column() == 6:  # 编辑按钮
             return self.create_edit_widget(parent, index.row())
+
+
         return super().createEditor(parent, option, index)
+
+    def destroyEditor(self, editor, index):
+        # 用来通知哪个单元格需要销毁编辑器
+        if index.column() == 0:  # 只处理第一列的编辑器
+            super().destroyEditor(editor, index)
+            self.signals.destroyEditor.emit(index.row(), index.column())
 
     def paint(self, painter, option, index):
         # 重写paint方法，不执行任何绘制操作
         pass
+
+    # def paint(self, painter, option, index):
+    """
+    在使用openPersistentEditor时会持久化创建编辑器，就不需要使用print函数了
+    使用print函数会导致窗口闪烁
+    """
+
+    #     # 绘制单元格
+    #     timer = QElapsedTimer()
+    #     timer.start()
+    #
+    #     table_view = self.parent()
+    #     visible_range = table_view.viewport().rect()
+    #     row_height = table_view.rowHeight(0)
+    #
+    #     # 计算可见行的范围
+    #     first_visible_row = table_view.rowAt(visible_range.top())
+    #     last_visible_row = table_view.rowAt(visible_range.bottom())
+    #     config.logger.debug(f"index: {index.row()}, first_visible_row: {first_visible_row}, last_visible_row: {last_visible_row}")
+    #     if index.column() in [0, 1, 2, 3, 4, 5, 6]:
+    #         if index not in self._persistent_editors:
+    #             editor = self.createEditor(self.parent(), option, index)
+    #             self.setEditorData(editor, index)
+    #             self.parent().setIndexWidget(index, editor)
+    #             self._persistent_editors[index] = editor
+    #     else:
+    #         super().paint(painter, option,index)
+    #         # print('======')
+    #         # print(len(self._persistent_editors))
+    #         # print(self._persistent_editors)
+    #         # print(f"Paint time: {timer.elapsed()} ms")
 
     def create_checkbox(self, parent):
         return CheckBox(parent)
@@ -251,9 +247,59 @@ class CustomItemDelegate(QStyledItemDelegate):
 
 class SubtitleModel(QAbstractTableModel):
     # 定义数据模型
-    def __init__(self, data, parent=None):
+    def __init__(self, file_path, parent=None):
         super().__init__(parent)
-        self._data = data
+        self.file_path = file_path
+        self._data = self.load_subtitle()
+
+    def load_subtitle(self):
+        """
+        加载字幕文件，返回字幕列表
+        :return:[('00:00:00,166', '00:00:01,166', '你好，世界！')]
+                [start_time, end_time, content]
+        """
+        subtitles = []
+        with open(self.file_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+
+        i = 0
+        while i < len(lines):
+            # 跳过行号
+            if lines[i].strip().isdigit():
+                i += 1
+
+            # 读取时间范围
+            time_range = lines[i].strip()
+            try:
+                start_time, end_time = time_range.split(' --> ')
+            except ValueError:
+                config.logger.error(f"Error parsing time range: {time_range}")
+                i += 1
+                continue
+            i += 1
+
+            # 读取字幕内容
+            english_content = []
+            if i < len(lines) and lines[i].strip():
+                english_content.append(lines[i].strip())
+                i += 1
+
+            chinese_content = []
+            if i < len(lines) and lines[i].strip():
+                chinese_content.append(lines[i].strip())
+                i += 1
+
+            english_content_str = ' '.join(english_content)
+            chinese_content_str = ' '.join(chinese_content)
+
+            # 添加到字幕列表
+            subtitles.append((start_time, end_time, english_content_str, chinese_content_str))
+
+            # 跳过空行
+            while i < len(lines) and not lines[i].strip():
+                i += 1
+        config.logger.debug(f"字幕文件行数: {len(subtitles)}")
+        return subtitles
 
     def rowCount(self, parent=QModelIndex()):
         return len(self._data)
@@ -305,27 +351,45 @@ class SubtitleModel(QAbstractTableModel):
         return super().flags(index) | Qt.ItemIsEditable
 
 
+class VirtualScrollSignals(QObject):
+    # 使用 VirtualScrollDelegate 来管理编辑器的创建和销毁。
+    createEditor = Signal(int, int)
+    destroyEditor = Signal(int, int)
+
+
 class SubtitleTable(QTableView):
-    # 定义表格视图
+    """
+    这个实现的主要特点：
+        1.使用 VirtualScrollDelegate 来管理编辑器的创建和销毁。
+        2.SubtitleTable 类现在维护一个 visible_editors 集合来跟踪当前可见的编辑器。
+        3.create_visible_editors 方法只为可见区域创建编辑器。
+        4.remove_invisible_editors 方法移除不再可见的编辑器。
+        5.on_scroll 方法在滚动时被调用，更新可见的编辑器。
+        6.initialize_visible_editors 方法在表格初始化时创建初始可见的编辑器。
+    这种实现的优点：
+        1.大大减少了内存使用，因为只有可见的单元格才会创建编辑器。
+        2.提高了性能，特别是对于大型数据集。
+        3.保持了良好的用户体验，因为编辑器会动态创建和销毁。
+
+    遗留问题：当前初始化时会同时调用：resizeEvent和initialize_visible_editors，
+    导致编辑器创建函数执行2次，但是好的是会去set（visible_editors）中查找，实际编辑器还只创建了一个
+
+    """
     def __init__(self, file_path: str):
         super().__init__()
-        self.model = None
         self.file_path = file_path
-        self.data_loader = DataLoader(file_path)
-        self.data_loader.data_loaded.connect(self.on_data_loaded)
-        self.data_loader.start()
+        self.visible_editors = set()
+        self.on_data_loaded()
 
-        # 添加计时器
-        self.update_timer = QTimer(self)
-        self.update_timer.setSingleShot(True)
-        self.update_timer.timeout.connect(self.delayed_update)
-
-    def on_data_loaded(self, data):
-        self.all_data = data
-        self.model = SubtitleModel(self.all_data)
+    def on_data_loaded(self):
+        self.model = SubtitleModel(self.file_path)
         self.setModel(self.model)
 
-        self.setItemDelegate(CustomItemDelegate(self))
+        self.delegate = CustomItemDelegate(self)
+        self.setItemDelegate(self.delegate)
+
+        self.delegate.signals.createEditor.connect(self.on_editor_created)
+        self.delegate.signals.destroyEditor.connect(self.on_editor_destroyed)
 
         self.setVerticalScrollMode(QTableView.ScrollPerPixel)
         self.setHorizontalScrollMode(QTableView.ScrollPerPixel)
@@ -336,30 +400,134 @@ class SubtitleTable(QTableView):
         for col, width in enumerate(column_widths):
             self.setColumnWidth(col, width)
 
-        for row in range(self.model.rowCount()):
+        # self.setEditTriggers(QTableView.NoEditTriggers)
+        # self.setSelectionMode(QTableView.NoSelection)
+
+        # # 连接滚动信号
+        self.verticalScrollBar().valueChanged.connect(self.on_scroll)
+        self.horizontalScrollBar().valueChanged.connect(self.on_scroll)
+
+        QTimer.singleShot(0, self.initialize_visible_editors)
+
+    def initialize_visible_editors(self):
+        # initialize_visible_editors 方法在表格初始化时创建初始可见的编辑器。
+        config.logger.debug("Initializing visible event")
+        self.create_visible_editors()
+
+    def create_visible_editors(self):
+        # create_visible_editors 方法只为可见区域创建编辑器。
+        visible_rect = self.viewport().rect()
+        top_left = self.indexAt(visible_rect.topLeft())
+        bottom_right = self.indexAt(visible_rect.bottomRight())
+        config.logger.debug(f"Visible rect: {visible_rect}")
+        config.logger.debug(f"Top left index: {top_left.row()}")
+        config.logger.debug(f"Bottom right index: {bottom_right.row()}")
+        
+        # if not top_left.isValid() or not bottom_right.isValid():
+        #     config.logger.warning("Invalid index range")
+        #     return
+
+        # config.logger.debug(f"Creating editors for rows {top_left.row()} to {bottom_right.row()}")
+
+
+        # 确保我们至少创建一行编辑器，即使表格行数少于窗口高度
+        start_row = 0 if not top_left.isValid() else top_left.row()
+        end_row = self.model.rowCount() - 1 if not bottom_right.isValid() else bottom_right.row()
+
+        # 确保 end_row 不小于 start_row
+        end_row = max(start_row, end_row)
+
+        config.logger.debug(f"Creating editors for rows {start_row} to {end_row}")
+        config.logger.debug(f"Visible editors: {self.visible_editors}")
+
+        # for row in range(start_row, end_row + 1):
+        #     if row < self.model.rowCount():  # 确保不超出模型的行数
+        #         for col in range(self.model.columnCount()):
+        #             if (row, col) not in self.visible_editors:
+        #                 index = self.model.index(row, col)
+        #                 self.openPersistentEditor(index)
+        #                 config.logger.debug(f"Created editor for ({row}, {col})")
+        for row in range(start_row, end_row + 1):
+            if row < self.model.rowCount():  # 确保不超出模型的行数
+                if (row, 0) not in self.visible_editors:
+                    index = self.model.index(row, 0)
+                    self.openPersistentEditor(index)
+                    config.logger.debug(f"Created editor for ({row}, 0)")
+
+
+        self.verify_visible_editors()
+
+    def remove_invisible_editors(self):
+        # remove_invisible_editors 方法移除不再可见的编辑器。
+        # 当前如果使用该函数，向下滚动时会销毁编辑器，但是往回滚动时不会重新创建编辑器。所以先屏蔽掉
+        visible_rect = self.viewport().rect()
+        top_left = self.indexAt(visible_rect.topLeft())
+        bottom_right = self.indexAt(visible_rect.bottomRight())
+
+        if not top_left.isValid() or not bottom_right.isValid():
+            config.logger.warning("Invalid index range")
+            return
+
+        visible_range = set((row, col) for row in range(top_left.row(), bottom_right.row() + 1) for col in range(self.model.columnCount()))
+
+        to_remove = self.visible_editors - visible_range
+        for row, col in to_remove:
+            index = self.model.index(row, col)
+            self.closePersistentEditor(index)
+            config.logger.debug(f"Removed editor for ({row}, {col})")
+
+
+        # visible_rect = self.viewport().rect()
+        # top_left = self.indexAt(visible_rect.topLeft())
+        # bottom_right = self.indexAt(visible_rect.bottomRight())
+        #
+        # if not top_left.isValid() or not bottom_right.isValid():
+        #     config.logger.warning("Invalid index range")
+        #     return
+        #
+        # visible_range = set((row, 0) for row in range(top_left.row(), bottom_right.row() + 1))
+        #
+        # to_remove = self.visible_editors - visible_range
+        # for row, col in to_remove:
+        #     index = self.model.index(row, col)
+        #     self.closePersistentEditor(index)
+        #     config.logger.debug(f"Removed editor for ({row}, {col})")
+
+    def on_scroll(self):
+        # on_scroll 方法在滚动时被调用，更新可见的编辑器。
+        config.logger.debug("Scroll event")
+        self.create_visible_editors()
+        # self.remove_invisible_editors()
+
+    def on_editor_created(self, row, col):
+        self.visible_editors.add((row, col))
+
+    def on_editor_destroyed(self, row, col):
+        self.visible_editors.discard((row, col))
+
+    def verify_visible_editors(self):
+        visible_rect = self.viewport().rect()
+        top_left = self.indexAt(visible_rect.topLeft())
+        bottom_right = self.indexAt(visible_rect.bottomRight())
+
+        if not top_left.isValid() or not bottom_right.isValid():
+            return
+
+        for row in range(top_left.row(), bottom_right.row() + 1):
             for col in range(self.model.columnCount()):
-                self.openPersistentEditor(self.model.index(row, col))
+                if (row, col) not in self.visible_editors:
+                    config.logger.warning(f"Editor missing for visible cell ({row}, {col})")
+                    self.openPersistentEditor(self.model.index(row, col))
 
+    def showEvent(self, event):
+        super().showEvent(event)
+        QTimer.singleShot(0, self.create_visible_editors)
 
-
-
-    def schedule_update(self):
-        # 使用计时器来延迟更新，避免频繁更新
-        self.update_timer.start(500)  # 100ms 延迟
-
-    def delayed_update(self):
-        # 实际更新视口
-        self.viewport().update()
-
-    def closeEvent(self, event):
-        # 确保在关闭表格时清理所有编辑器
-        delegate = self.itemDelegate()
-        if isinstance(delegate, CustomItemDelegate):
-            for editor in delegate._persistent_editors.values():
-                editor.deleteLater()
-        super().closeEvent(event)
-
-    # def _add_row(self, srt_data):  #     # 初始化时加载数据  #     for row_position, srt_line in enumerate(srt_data):  #         # 第四列：时间  #         index_3 = self.model.index(row_position, 3)  # 第4列的索引是3  #         start_time = srt_line[0]  #         end_time = srt_line[1]  #         times = (start_time, end_time)  #         self.model.setData(index_3, times, Qt.UserRole)  #         # 第五列：原文  #         index_4 = self.model.index(row_position, 4)  # 第5列的索引是4  #         self.model.setData(index_4, srt_line[2], Qt.EditRole)
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        config.logger.debug("Resize event")
+        self.create_visible_editors()
+        # self.remove_invisible_editors()
 
 
 if __name__ == "__main__":
