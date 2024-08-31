391576a078e25b12a3ce29cff1667b02bba2dd11
riftcover <gaoshoupan@hotmail.com>
2024-08-31 15:31:01 +0800

    feat(srt_edit.py): 修改右侧视频布局永远锁定纵横比16：9
    调整视频比例为16：9+50像素，保证在左右拖拽窗口时，视频组件纵横比固定
    调整init_ui接口，更易读

--- nice_ui/ui/srt_edit.py
+++ nice_ui/ui/srt_edit.py
@@ -2,6 +2,7 @@ import sys
 
 from PySide6 import QtWidgets
 from PySide6.QtCore import QUrl, Qt
+from PySide6.QtGui import QColor
 from PySide6.QtWidgets import (QApplication, QVBoxLayout, QHBoxLayout, QSpacerItem, QSizePolicy, QSplitter, QWidget)
 
 from nice_ui.widget.subedit import SubtitleTable
@@ -10,91 +11,99 @@ from vendor.qfluentwidgets import CardWidget, ToolTipFilter, ToolTipPosition, Tr
 from vendor.qfluentwidgets.multimedia import LinVideoWidget
 
 
+class AspectRatioWidget(QWidget):
+    # 保持视频部件的宽高比
+    def __init__(self, widget, aspect_ratio):
+        super().__init__()
+        self.aspect_ratio = aspect_ratio
+        layout = QVBoxLayout(self)
+        layout.addWidget(widget)
+
+    def resizeEvent(self, event):
+        # 视频媒体部分为16：9，底部bar自身高度为40，
+        # 为了保证在编辑器中bar不会超出媒体部分，所以+50
+        width = self.width()
+        height = int(width / self.aspect_ratio)+50
+        self.setFixedHeight(height)
+        super().resizeEvent(event)
+
 class SubtitleEditPage(CardWidget):
     def __init__(self, patt):
         super().__init__()
         self.patt = patt
+        self.subtitle_table = SubtitleTable(self.patt)
         self.initUI()
 
     def initUI(self):
-        # 创建主布局
         main_layout = QHBoxLayout(self)
+        splitter = QSplitter(Qt.Horizontal)
+        main_layout.addWidget(splitter)
 
+        # 左侧部分
+        left_widget = self.create_left_widget()
+        splitter.addWidget(left_widget)
+
+        # 右侧部分
+        right_widget = self.create_right_widget()
+        splitter.addWidget(right_widget)
+
+        # 设置分割器比例
+        splitter.setStretchFactor(0, 2)  # 左侧部件占2
+        splitter.setStretchFactor(1, 1)  # 右侧部件占1
+
+    def create_left_widget(self):
         left_widget = QWidget()
         layout = QVBoxLayout(left_widget)
 
-        # 创建分割器
-        splitter = QSplitter(Qt.Horizontal)
-        splitter.setFrameShape(QtWidgets.QFrame.NoFrame)
-        splitter.setFrameShadow(QtWidgets.QFrame.Sunken)
-        splitter.setLineWidth(2)
-        subtitle_table = SubtitleTable(self.patt)
+        # 顶部卡片
+        top_card = self.create_top_card()
+        layout.addWidget(top_card)
 
-        # 创建并添加CardWidget
+        # 字幕表格
+
+        layout.addWidget(self.subtitle_table)
+
+        return left_widget
+
+    def create_top_card(self):
         top_card = CardWidget()
         top_layout = QHBoxLayout(top_card)
         top_layout.setSpacing(10)
         top_layout.setContentsMargins(10, 10, 10, 10)
 
-        export_button = TransparentToolButton(FluentIcon.EMBED)
-        export_button.clicked.connect(self.export_srt)
-        save_button = TransparentToolButton(FluentIcon.SAVE)
-        save_button.clicked.connect(subtitle_table.save_subtitle)
+        # 添加按钮
+        buttons = [
+            (TransparentToolButton(FluentIcon.DOWN), self.subtitle_table.move_row_down_more, "将勾选的译文整体向下移动"),
+            (TransparentToolButton(FluentIcon.UP), self.subtitle_table.move_row_up_more, "将勾选的译文向上移动译文"),
+            (TransparentToolButton(FluentIcon.SAVE), self.subtitle_table.save_subtitle, "保存到本地，以免丢失"),
+            (TransparentToolButton(FluentIcon.EMBED), self.export_srt, "导出srt格式字幕文件"),
+        ]
 
-        # 向下移动按钮
-        down_rows = TransparentToolButton(FluentIcon.DOWN)
-        down_rows.clicked.connect(subtitle_table.move_row_down_more)
-        # 向上移动按钮
-        up_rows = TransparentToolButton(FluentIcon.UP)
-        up_rows.clicked.connect(subtitle_table.move_row_up_more)
-        # 导入译文
-
-        import_button = TransparentToolButton(FluentIcon.LABEL)
-        import_button.clicked.connect(self.import_subtitle)
-
-        # 从右向左排列按钮
         top_layout.addStretch(1)
-        # top_layout.addWidget(import_button)
-        top_layout.addWidget(down_rows)
-        top_layout.addWidget(up_rows)
-        top_layout.addWidget(save_button)
-        top_layout.addWidget(export_button)
+        for button, callback, tooltip in buttons:
+            button.clicked.connect(callback)
+            button.setToolTip(tooltip)
+            button.installEventFilter(ToolTipFilter(button, showDelay=300, position=ToolTipPosition.BOTTOM_RIGHT))
+            top_layout.addWidget(button)
 
+        return top_card
+
+    def create_right_widget(self):
         right_widget = QWidget()
         video_layout = QVBoxLayout(right_widget)
-        self.videoWidget = LinVideoWidget(subtitle_table,subtitle_table.subtitles,self)
-        # self.videoWidget.setVideo(QUrl('/Users/locodol/my_own/code/lin_trans/result/tt1/vv2.mp4'))
+        video_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.videoWidget = LinVideoWidget(self.subtitle_table, self.subtitle_table.subtitles, self)
         self.videoWidget.setVideo(QUrl('D:/dcode/lin_trans/result/tt1/tt.mp4'))
-        spacer = QSpacerItem(20, 500, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        video_container = AspectRatioWidget(self.videoWidget, 16/9)
 
-        layout.addWidget(top_card)
-        layout.addWidget(subtitle_table)
+        video_layout.addWidget(video_container)
+        video_layout.addItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
 
-        video_layout.addWidget(self.videoWidget)
-        video_layout.addItem(spacer)
-
-        # 将左右部件添加到分割器
-        splitter.addWidget(left_widget)
-        splitter.addWidget(right_widget)
-        # 设置左侧占2/3，右侧占1/3
-        splitter.setStretchFactor(0, 2)  # 左侧部件索引为0，比例为2
-        splitter.setStretchFactor(1, 1)  # 右侧部件索引为1，比例为1
-
-        main_layout.addWidget(splitter)
+        # 视频部件
 
 
-        export_button.setToolTip("导出srt格式字幕文件")
-        export_button.installEventFilter(ToolTipFilter(export_button, showDelay=300, position=ToolTipPosition.BOTTOM_RIGHT))
-        save_button.setToolTip("保存到本地，以免丢失")
-        save_button.installEventFilter(ToolTipFilter(save_button, showDelay=300, position=ToolTipPosition.BOTTOM_RIGHT))
-        down_rows.setToolTip("将勾选的译文整体向下移动")
-        down_rows.installEventFilter(ToolTipFilter(down_rows, showDelay=300, position=ToolTipPosition.BOTTOM_RIGHT))
-        up_rows.setToolTip("将勾选的译文向上移动译文")
-        up_rows.installEventFilter(ToolTipFilter(up_rows, showDelay=300, position=ToolTipPosition.BOTTOM_RIGHT))
-        import_button.setToolTip("导入译文")
-        import_button.installEventFilter(ToolTipFilter(import_button, showDelay=300, position=ToolTipPosition.BOTTOM_RIGHT))
-
-
+        return right_widget
 
     def export_srt(self):
         # todo：导出srt文件
