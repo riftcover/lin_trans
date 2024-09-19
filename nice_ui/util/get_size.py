dbaa3eac3c6b8f63086131a9402a98a6c8202847
riftcover <gaoshoupan@hotmail.com>
2024-09-19 21:01:50 +0800

    refactor: 获取widget实例名称

--- nice_ui/util/get_size.py
+++ nice_ui/util/get_size.py
@@ -1,14 +1,14 @@
 from PySide6.QtGui import QFontMetrics
 
-from nice_ui.configure.config import logger
+from utils import logger
 
 
 def get_font_size(weight):
     font = weight.font()
     font_hight = QFontMetrics(font).height()
-    logger.debug(f" {weight}font_hight: {font_hight}")
+    logger.debug(f" {weight.__class__.__name__}font_hight: {font_hight}")
 
 
 def get_widget_size(widget):
     size = widget.sizeHint()
-    logger.debug(f"{widget} sizeHint: {size}")
+    logger.debug(f"{widget.__class__.__name__} sizeHint: {size}")
