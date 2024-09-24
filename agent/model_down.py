263b0b411aa2e0001f63b26bd47d6c01a543f1df
riftcover <44368727+riftcover@users.noreply.github.com>
2024-09-24 16:47:16 +0800

    模型下载制定model目录

--- agent/model_down.py
+++ agent/model_down.py
@@ -1,9 +1,20 @@
 #模型下载
 from modelscope import snapshot_download
-# paraformer-zh
-# model_dir = snapshot_download('iic/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch')
-# SenseVoiceSmall
-# model_dir = snapshot_download('iic/SenseVoiceSmall')
+from nice_ui.configure import config
+import os
 
-#模型下载
-model_dir = snapshot_download('iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch')
+
+#模型下载-中文
+def download_model(model_name:str):
+    download_path = os.path.join(config.root_path, 'models', 'funasr')
+    os.makedirs(download_path, exist_ok=True)
+    snapshot_download(model_name,cache_dir=download_path)
+
+download_model('iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch')
+
+# #模型下载-英语
+#
+# model_dir = snapshot_download('iic/speech_paraformer-large-vad-punc_asr_nat-en-16k-common-vocab10020')
+#
+# #模型下载-全语言
+# model_dir = snapshot_download('iic/SenseVoiceSmall')
