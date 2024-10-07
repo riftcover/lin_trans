f744e7cafa5d98267bf723dfb27838a98d19099d
riftcover <gaoshoupan@hotmail.com>
2024-10-07 11:37:18 +0800

    1.添加Win32

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 import sys
-
+import os
 from PySide6.QtWidgets import QApplication
 
 from nice_ui.ui.MainWindow import Window
+
+
+# 将当前目录添加到 sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, current_dir)
+
+# 打印 sys.path 以进行调试
+print("sys.path:", sys.path)
 
 app = QApplication(sys.argv)
 w = Window()
