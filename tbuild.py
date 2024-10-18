78b72461a84c60b91939012b2e0b3d810910c25d
riftcover <gaoshoupan@hotmail.com>
2024-10-18 16:11:25 +0800

    调整打包脚本

--- tbuild.py
+++ tbuild.py
@@ -1,56 +1,22 @@
-import os
-import subprocess
-import sys
-import shutil
+# import os
+# import shutil
 
-# 清理旧的构建文件
-if os.path.exists("dist"):
-    shutil.rmtree("dist")
-if os.path.exists("build"):
-    shutil.rmtree("build")
+# def remove_docs_and_tests(directory):
+#     for root, dirs, files in os.walk(directory):
+#         for dir in dirs:
+#             if dir in ['doc', 'docs', 'test', 'tests']:
+#                 shutil.rmtree(os.path.join(root, dir))
 
-def run_pyinstaller_build():
-    # 获取当前脚本的目录
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-
-    # 主Python文件名
-    main_file = "run.py"
-
-    # PyInstaller命令
-    pyinstaller_command = [
-        "pyinstaller",
-        "--name=linlin",
-        "--windowed",
-        "--onedir",
-        "--add-data=nice_ui/language/*.json:nice_ui/language",
-        "--hidden-import=scipy",
-        "--hidden-import=numpy",
-        "--hidden-import=better_ffmpeg_progress",
-        "--hidden-import=colorthief",
-        "--hidden-import=darkdetect",
-        "--hidden-import=httpx",
-        "--hidden-import=loguru",
-        "--hidden-import=modelscope",
-        "--hidden-import=openai",
-        "--hidden-import=packaging",
-        "--hidden-import=path",
-        "--hidden-import=pydantic",
-        "--hidden-import=sqlalchemy",
-        "--hidden-import=socksio",
-        "--add-data=utils:utils",
-        main_file
-        ]
-
-    print("开始 PyInstaller 构建...")
-    # 运行PyInstaller命令
-    try:
-        subprocess.run(pyinstaller_command, check=True)
-        print("PyInstaller 构建成功完成。")
-        print(f"可执行文件位于: {os.path.join(script_dir, 'dist', 'linlin')}")
-    except subprocess.CalledProcessError as e:
-        print(f"PyInstaller 构建失败：{e}")
-        sys.exit(1)
+# def remove_pyc_files(directory):
+#        for root, dirs, files in os.walk(directory):
+#            for file in files:
+#                if file.endswith('.pyc'):
+#                    os.remove(os.path.join(root, file))
+# dir_path = r'E:\tool\PyStand-py310-x64\site-packages'
+# remove_docs_and_tests(dir_path)
+# remove_pyc_files(dir_path)
 
 
-if __name__ == "__main__":
-    run_pyinstaller_build()
+from modulegraph import modulegraph
+graph = modulegraph.ModuleGraph()
+graph.run_script("run.py")
