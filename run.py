9f87df0ce2942bbb04e537f4648e374a15d2ce8e
riftcover <44368727+riftcover@users.noreply.github.com>
2024-11-16 19:22:04 +0800

    fix:1.mac系统上打包程序会重复启动，在run.py中添加multiprocessing.freeze_support()

--- run.py
+++ run.py
@@ -1,16 +1,21 @@
 import sys
 
+import multiprocessing
+
+from PySide6.QtCore import QSettings
 from PySide6.QtWidgets import QApplication
 from nice_ui.ui.MainWindow import Window
+from nice_ui.ui.video2srt import Video2SRT
 
 
 def main():
     app = QApplication(sys.argv)
-    # window = Video2SRT("字幕翻译", settings=QSettings("Locoweed", "LinLInTrans"))
-    window = Window()
+    window = Video2SRT("字幕翻译", settings=QSettings("Locoweed", "LinLInTrans"))
+    # window = Window()
     window.show()
     sys.exit(app.exec())
 
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     main()
