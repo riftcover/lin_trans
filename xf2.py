bf74454c8b2701976cba5a2712fa7e8adb1544c4
riftcover <44368727+riftcover@users.noreply.github.com>
2025-01-16 16:55:48 +0800

    remove xf2.py

--- xf2.py
+++
@@ -1,4 +0,0 @@
-#模型下载
-from modelscope import snapshot_download
-# model_dir = snapshot_download('iic/speech_campplus_sv_zh-cn_16k-common')
-model_dir = snapshot_download('iic/punc_ct-transformer_cn-en-common-vocab471067-large')
