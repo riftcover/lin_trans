5f4ebd66e6565164434f4964971cc5ca69a77af6
riftcover <gaoshoupan@hotmail.com>
2025-02-10 23:26:48 +0800

    1.添加个人中心页面

--- nice_ui/ui/profile.py
+++ nice_ui/ui/profile.py
@@ -45,25 +45,80 @@ class ProfileInterface(QFrame):
         # 邮箱地址
         self.emailLayout = QHBoxLayout()
         self.emailIcon = IconWidget(FIF.MAIL, self)
+        
+        # 创建垂直布局来包含邮箱标签和值
+        self.emailContentLayout = QVBoxLayout()
         self.emailLabel = BodyLabel('邮箱地址', self)
         self.emailValue = BodyLabel('user@example.com', self)
+        
+        # 创建一个容器来包含邮箱值，以便添加下划线
+        self.emailValueContainer = QFrame()
+        self.emailValueContainer.setObjectName('emailValueContainer')
+        self.emailValueLayout = QHBoxLayout(self.emailValueContainer)
+        self.emailValueLayout.setContentsMargins(0, 0, 0, 8)  # 底部留出空间给下划线
+        self.emailValueLayout.addWidget(self.emailValue)
+        
+        self.emailValue.setStyleSheet('font-size: 16px; color: #1a1a1a;')
+        self.emailLabel.setStyleSheet('color: #666666;')
+        self.emailValueContainer.setStyleSheet("""
+            #emailValueContainer {
+                border-bottom: 1px solid #DDDDDD;
+            }
+        """)
+        
+        # 将标签和值容器添加到垂直布局中
+        self.emailContentLayout.addWidget(self.emailLabel)
+        self.emailContentLayout.addWidget(self.emailValueContainer)
+        self.emailContentLayout.setSpacing(4)
+        
+        # 将图标和垂直布局添加到水平布局中
         self.emailLayout.addWidget(self.emailIcon)
-        self.emailLayout.addWidget(self.emailLabel)
+        self.emailLayout.addLayout(self.emailContentLayout)
         self.emailLayout.addStretch()
-        self.emailLayout.addWidget(self.emailValue)
         self.accountLayout.addLayout(self.emailLayout)
+        
+        # 添加一些间距
+        self.accountLayout.addSpacing(10)
         
         # 算力额度
         self.quotaLayout = QHBoxLayout()
         self.quotaIcon = IconWidget(FIF.SPEED_HIGH, self)
+        
+        # 创建垂直布局来包含算力标签和值
+        self.quotaContentLayout = QVBoxLayout()
         self.quotaLabel = BodyLabel('算力额度', self)
+        self.quotaLabel.setStyleSheet('color: #666666;')
+        
+        # 创建一个容器来包含值和单位，以便添加下划线
+        self.quotaValueContainer = QFrame()
+        self.quotaValueContainer.setObjectName('quotaValueContainer')
+        self.quotaValueLayout = QHBoxLayout(self.quotaValueContainer)
+        self.quotaValueLayout.setContentsMargins(0, 0, 0, 8)  # 底部留出空间给下划线
+        
         self.quotaValue = SubtitleLabel('1000', self)
         self.quotaUnit = BodyLabel('点数', self)
+        self.quotaValue.setStyleSheet('font-size: 16px; color: #1a1a1a;')
+        
+        self.quotaValueLayout.addWidget(self.quotaValue)
+        self.quotaValueLayout.addWidget(self.quotaUnit)
+        self.quotaValueLayout.setSpacing(4)
+        self.quotaValueLayout.addStretch()
+        
+        self.quotaValueContainer.setStyleSheet("""
+            #quotaValueContainer {
+                border-bottom: 1px solid #DDDDDD;
+            }
+        """)
+        
+        # 将标签和值容器添加到垂直布局中
+        self.quotaContentLayout.addWidget(self.quotaLabel)
+        self.quotaContentLayout.addWidget(self.quotaValueContainer)
+        self.quotaContentLayout.setSpacing(4)
+        
+        # 将图标和垂直布局添加到水平布局中
         self.quotaLayout.addWidget(self.quotaIcon)
-        self.quotaLayout.addWidget(self.quotaLabel)
+        self.quotaLayout.addLayout(self.quotaContentLayout)
         self.quotaLayout.addStretch()
-        self.quotaLayout.addWidget(self.quotaValue)
-        self.quotaLayout.addWidget(self.quotaUnit)
         self.accountLayout.addLayout(self.quotaLayout)
         
         # 当前剩余可用算力提示
@@ -73,7 +128,6 @@ class ProfileInterface(QFrame):
         
         # 购买算力按钮
         self.buyButton = PushButton('购买算力', self)
-        self.buyButton.setIcon(FIF.ADD)
         self.accountLayout.addWidget(self.buyButton)
         
         self.vBoxLayout.addWidget(self.accountCard)
@@ -94,41 +148,3 @@ class ProfileInterface(QFrame):
         
         self.vBoxLayout.addWidget(self.usageCard)
         self.vBoxLayout.addStretch()
-        
-        # 设置样式
-        self.setStyleSheet("""
-            QFrame {
-                background-color: rgb(243, 243, 243);
-            }
-            
-            SimpleCardWidget {
-                background-color: white;
-                border: none;
-                border-radius: 10px;
-                padding: 20px;
-            }
-            
-            SubtitleLabel {
-                font-size: 18px;
-                font-weight: bold;
-                padding-bottom: 10px;
-            }
-            
-            BodyLabel {
-                font-size: 14px;
-            }
-            
-            PrimaryPushButton {
-                border-radius: 5px;
-                padding: 8px 16px;
-                font-size: 14px;
-            }
-            
-            PushButton {
-                border: 1px solid #DDDDDD;
-                border-radius: 5px;
-                padding: 8px 16px;
-                font-size: 14px;
-                margin-top: 10px;
-            }
-        """)
