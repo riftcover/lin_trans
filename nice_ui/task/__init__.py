4a6903bc5f35694733df1d1f6fd45966937df398
riftcover <gaoshoupan@hotmail.com>
2025-04-22 12:27:45 +0800

    "实现ORM工厂模式，优化WORK_TYPE与ORM的映射关系"

--- nice_ui/task/__init__.py
+++ nice_ui/task/__init__.py
@@ -2,6 +2,7 @@ from enum import IntEnum
 
 
 class WORK_TYPE(IntEnum):
+    # 添加新的工作类型时，同时需要orm_factory.py中添加相应的映射关系（get_orm_by_work_type和set_orm_job_status方法）
     ASR = 1
     TRANS = 2
     ASR_TRANS = 3
